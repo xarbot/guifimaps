6bcd00fe49bfce3b2d8c5af78454f084dd2b47f0
Oriol Rico <oriol.rico@gmail.com>
2017-03-30 11:27:55 +0200

    generated files on 30-03-2017 11:27:55

--- parsers/snmp.php
+++ parsers/snmp.php
@@ -17,29 +17,40 @@
 		$uid=$row2["uid"];
 
                 $session = new SNMP(SNMP::VERSION_1, substr($row2['ip'], 0, -3), "public");
+		$fulltree = $session->walk(".");
 		//$session = new SNMP(SNMP::VERSION_1, "10.0.30.1", "public");
-                $fulltree = $session->walk(".");
-                //print_r($fulltree);
-		list($brossa,$uptime)=explode(")",$fulltree["DISMAN-EVENT-MIB::sysUpTimeInstance"]);
-		//echo $uptime;
-		//exit();
-		$uptime=trim($uptime);
-		echo $uptime."\n";
-                $session->close();
+		if ($fulltree["DISMAN-EVENT-MIB::sysUpTimeInstance"]==""){
+			//Provem port alternatiu i si no descartem
+			$session = new SNMP(SNMP::VERSION_1, substr($row2['ip'], 0, -3).":170", "public");
+			$fulltree = $session->walk(".");
+			echo "SNMP sense resposta, busco port alternatiu\n";
+		}
+		if ($fulltree["DISMAN-EVENT-MIB::sysUpTimeInstance"]!=""){
+                	//$fulltree = $session->walk(".");
+	                //print_r($fulltree);
+			list($brossa,$uptime)=explode(")",$fulltree["DISMAN-EVENT-MIB::sysUpTimeInstance"]);
+			//echo $uptime;
+			//exit();
+			$uptime=trim($uptime);
+			echo $uptime."\n";
+	                $session->close();
+	
+			$sql3 = "update nodes set uptime='".$uptime."' where uid='".$uid."' and timestamp_captura='".$last_timestamp."'";
+			$result3 = $mysqli->query($sql3);
 
-		$sql3 = "update nodes set uptime='".$uptime."' where uid='".$uid."' and timestamp_captura='".$last_timestamp."'";
-		$result3 = $mysqli->query($sql3);
-
-		$i=1;
-		$num_interficies=str_replace("INTEGER: ","",$fulltree["IF-MIB::ifNumber.0"])+1;
-		while ($i<$num_interficies){
-			$nom_interficie=str_replace("STRING: ","",$fulltree["IF-MIB::ifDescr.".$i]);
-			$inbytes=str_replace("Counter32: ","",$fulltree["IF-MIB::ifInOctets.".$i]);
-			$outbytes=str_replace("Counter32: ","",$fulltree["IF-MIB::ifOutOctets.".$i]);
-			$sql4 = "insert into interface_volume(uid,interface,inbytes,outbytes,timestamp_captura) values ('".$uid."','".$nom_interficie."','".$inbytes."','".$outbytes."','".$last_timestamp."')";
-			echo $sql4."\n";
-			$result4 = $mysqli->query($sql4);
-			$i++;
+			$i=1;	
+			$num_interficies=str_replace("INTEGER: ","",$fulltree["IF-MIB::ifNumber.0"])+1;
+			while ($i<$num_interficies){
+				$nom_interficie=str_replace("STRING: ","",$fulltree["IF-MIB::ifDescr.".$i]);
+				$inbytes=str_replace("Counter32: ","",$fulltree["IF-MIB::ifInOctets.".$i]);
+				$outbytes=str_replace("Counter32: ","",$fulltree["IF-MIB::ifOutOctets.".$i]);
+				$sql4 = "insert into interface_volume(uid,interface,inbytes,outbytes,timestamp_captura) values ('".$uid."','".$nom_interficie."','".$inbytes."','".$outbytes."','".$last_timestamp."')";
+				echo $sql4."\n";
+				$result4 = $mysqli->query($sql4);
+				$i++;
+			}
+		}else{
+		     echo "S'ha produit un error: SNMP sense resposta.\n";
 		}
 
 		echo "--------------------------------------------------------------\n";
