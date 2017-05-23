6a9c10101f83885a9f875ca23715c0ef4c211d4b
Oriol Rico <oriol.rico@gmail.com>
2017-05-23 08:17:17 +0200

    generated files on 23-05-2017 08:17:16

--- functions.php
+++ functions.php
@@ -79,4 +79,29 @@ function distanceCalculation($point1_lat, $point1_long, $point2_lat, $point2_lon
 	}
 	return round($distance, $decimals);
 }
+
+function correlacio($x, $y){
+
+	$length= count($x);
+	$mean1=array_sum($x) / $length;
+	$mean2=array_sum($y) / $length;
+
+	$a=0;
+	$b=0;
+	$axb=0;
+	$a2=0;
+	$b2=0;
+	
+	for($i=0;$i<$length;$i++){
+		$a=$x[$i]-$mean1;
+		$b=$y[$i]-$mean2;
+		$axb=$axb+($a*$b);
+		$a2=$a2+ pow($a,2);
+		$b2=$b2+ pow($b,2);
+	}
+
+	$corr= $axb / sqrt($a2*$b2);
+	return $corr;
+}
+
 ?>
